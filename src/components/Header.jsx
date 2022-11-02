abd807698aae73c0938e42451a9a082d64f55eb9
FedeGastaldi <gastaldifederico69@gmail.com>
2022-11-02 07:56:24 -0300

    header img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             className="flex justify-center items-center bg-cover text-7xl bg-clip-text bg-center uppercase text-transparent font-extrabold tracking-wide antialiased font-['DM Sans']"
             style={{
               backgroundImage:
-                "url(src/img/asd.webp)",
+                "url(https://64.media.tumblr.com/63d37176cd23f66d598348ad8a2ef72a/c96407d011e7a33c-4c/s500x750/1cf876bdf86435a961d4e3a3e81888c50f6f01b4.gifv)",
             }}
           >
             Federico Gastaldi
